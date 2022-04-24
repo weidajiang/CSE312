543bceb95b056965b518955ad5d8e41d2ecac22a
zesenche <89846099+zesenche@users.noreply.github.com>
2022-04-24 08:47:15 -0400

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -1,11 +1,35 @@
-from flask import Flask,render_template
+import flask
+import bcrypt
+import hashlib
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
+
+@app.route('/', methods=["GET","POST"])
+def login():
+    app.secret_key = bcrypt.gensalt()  # 设置随机生成salt
+    if flask.request.method == 'GET':
+        return flask.render_template('index.html')
+    username = flask.request.form.get("username")  #获取表单里的username
+    password = flask.request.form.get("password")  #获取表单里的password
+    print(username)
+    print(password)
+    '''这里要判断用户名,hash值, salt是否存于database'''
+    if username == 'buxiangchizhaji' and password == "zhebeizi":
+        flask.session['user_info'] = username  #使用username加salt生成cookie
+        return flask.redirect("/user")
+
+    if True:
+        return flask.render_template("index.html", failed="wrong password or username")
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return render_template('index.html', contents = 'test')
+@app.route('/user')
+def userPage():
+    username = flask.session["user_info"]
+    return flask.render_template("UserPage.html", username=username)
+
+
+
+
 
 
 if __name__ == '__main__':
