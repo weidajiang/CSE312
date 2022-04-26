d5cb15bf1b60317234d328d0c44982332fcf5122
zesenche <89846099+zesenche@users.noreply.github.com>
2022-04-26 08:06:29 -0400

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -1,11 +1,10 @@
 import flask
 import bcrypt
 import os
-
-app = flask.Flask(__name__)
 from flask_sock import Sock
-
+app = flask.Flask(__name__)
 sock = Sock(app)
+clients = []
 
 
 #设置网页图标
@@ -13,6 +12,13 @@ sock = Sock(app)
 def favicon():
     return flask.send_from_directory(os.path.join(app.root_path, 'static'),
                           'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+
+#设置css
+@app.route('/style.css')
+def css_file():
+    return flask.send_from_directory(os.path.join(app.root_path, 'static'),
+                          'stye.css')
 
 
 @app.route('/', methods=["GET", "POST"])
@@ -32,11 +38,21 @@ def login():
     if True:
         return flask.render_template("index.html", failed="wrong password or username")
 
+
 #登入成功页面
 @app.route('/user')
 def userPage():
     username = flask.session["user_info"]
     return flask.render_template("UserPage.html", username=username)
+
+
+#访问meHotel
+@app.route('/kiwi')
+def kiwi():
+    with open("static/kiwi-removebg-preview.png", "rb") as f:
+        file = f.read()
+    return file
+
 
 #聊天室页面
 @app.route('/chat')
@@ -48,12 +64,21 @@ def chat():
 def static_dir(path):
     return flask.send_from_directory("static", path)
 
+
 #暂时模拟websocket, 响应牵手
 @sock.route('/websocket')
-def echo(sock):
-    while True:  
-        data = sock.receive()
-        sock.send(data)
+def websocket(socket):
+    clients.append(socket)
+    while True:
+        data = socket.receive()
+        data = data.replace("\r\n", "").replace("&", "&amp").replace(">", "&gt").replace("<", "&lt")
+        if data.__contains__("webRTC"):
+            for c in clients:
+                if c != socket:
+                    c.send(data)
+        else:
+            for c in clients:
+                c.send(data)
 
 
 
