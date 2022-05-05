2debb69047e2b3104c95e78a8d266a1bd1f321ed
zesenche <89846099+zesenche@users.noreply.github.com>
2022-05-04 22:33:09 -0400

    Wo git push bu liao! Cao!

--- webapp/MongoDB.py
+++ webapp/MongoDB.py
@@ -1,15 +1,17 @@
 import pymongo
 
-class mongoDB:
 
+class mongoDB:
     def __init__(self):
-        #mongodb://localhost:27017/
+        # mongodb://localhost:27017/
         client = pymongo.MongoClient("mongodb://localhost:27017/")
         db = client.Users
         InfoProject = db.InfoProject
         self.InfoProject = InfoProject
         Cookies = db.Cookies
         self.Cookies = Cookies
+        Profile = db.Profile
+        self.Profile = Profile
 
     def addInfo(self, username, password, salt):
 
@@ -27,10 +29,27 @@ class mongoDB:
                 "token": token}
         self.Cookies.insert_one(data)
 
-
     def findUsernameByCookie(self, cookie):
         result = self.Cookies.find_one({"token": cookie})
         return result
+
+    def addProfile(self, username, sex, year, address, bio):
+        data = {"username": username,
+                "sex": sex,
+                "year": year,
+                "address": address,
+                "bio": bio}
+        self.Profile.insert_one(data)
+
+    def UpdateProfile(self, username, sex, birth, address, bio):
+        new_value = {"$set": {"sex": sex, "year": birth, "address": address, "bio": bio}}
+        target = {"username": username}
+        self.Profile.update_one(target, new_value)
+
+    def findProfile(self, username):
+        result = self.Profile.find_one({"username": username})
+        return result
+
 
 
 
