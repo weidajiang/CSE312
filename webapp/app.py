b35bd9f556b19793d7997a0bd239f4d1029aeb1e
weidajiang <jiangwd1992@gmail.com>
2022-05-13 01:42:05 -0400

    add bootstrap modal to sign in and sign up

--- webapp/app.py
+++ webapp/app.py
@@ -260,5 +260,5 @@ def logout():
     return response
 
 if __name__ == '__main__':
-    #app.run(host="0.0.0.0", port = "8000")
-    app.run()
+    app.run(host="0.0.0.0", port = "8000")
+    #app.run()
